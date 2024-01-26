2f296f122a21dfac00940e124216e1cd5adfe530
shibil <shbl.salim@gmail.com>
2024-01-26 22:23:43 +0530

    Initial commit - Set up React Native project for MealsApp

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,18 @@
+import { Pressable, View,Text } from "react-native";
+
+
+function CategoryGridTile({title , color}){
+    return(
+    <View>
+        <Pressable>
+            <View>
+                <Text>{title}</Text>
+            </View>
+        </Pressable>
+
+    </View>
+        
+    );
+}
+
+export default CategoryGridTile;
