7091230755936357ef267163c97d7b235bad7b83
shibil <shbl.salim@gmail.com>
2024-01-27 21:40:29 +0530

    Implemented navigation between multiple screens.

--- screens/MealsViewScreen.js
+++ screens/MealsViewScreen.js
@@ -1,14 +1,29 @@
-import { View,Text,StyleSheet } from "react-native";
+import { View,Text,StyleSheet, FlatList } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealsViewScreen({route}){
 
    const catId = route.params.categoryId;
 
+   const displayedMeals =  MEALS.filter((mealItem)=>{
+    return mealItem.categoryIds.indexOf(catId) >=0;
+   });
+
+   function renderMealItem(itemData){
+    return(
+    <MealItem title={itemData.item.title} 
+        imageUrl={itemData.item.imageUrl}
+    />
+    );
+   }
+
 return(
         <View style={styles.container}>
-            <Text>Meals Overview Screen -{catId}</Text>
-        </View>
+         <FlatList data={displayedMeals}
+          keyExtractor={(item)=> item.id}
+          renderItem={renderMealItem}/>
+          </View>
 );
 }
 
