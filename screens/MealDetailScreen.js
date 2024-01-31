b1219dd38dca1415b7b3d9843591c46826b97bde
shibil <shbl.salim@gmail.com>
2024-01-31 23:30:08 +0530

    Update UI components with multiple adjustments

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,14 +1,30 @@
-import { View,Image,Text ,StyleSheet ,ScrollView} from "react-native";
+import { View,Image,Text ,StyleSheet ,ScrollView, Button} from "react-native";
+import { useLayoutEffect } from "react";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetail from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import IconButton from "../components/IconButton";
 
-function MealDetailScreen({route}){
+function MealDetailScreen({route , navigation}){
     const mealId=route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) =>meal.id === mealId);
+
+    function headerButtonPressHandler(){
+        console.log('Pressed!!');
+    }
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+        headerRight: ()=> {
+            return <IconButton 
+             icon="star" color="white" 
+            onPress={headerButtonPressHandler}/>;
+        }
+        });
+    },[navigation , headerButtonPressHandler]);
 
     return(
     <ScrollView style={styles.rootContainer}>
